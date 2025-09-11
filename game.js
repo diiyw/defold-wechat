ed876291896a5231810021af42990f24444f46ae
diiyw <zhuohong@live.com>
2025-09-12 01:17:53 +0800

    refactor(wasm):  file

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 import "./src/weapp-adapter/index.js";
 import { EngineLoader } from './dmloader';
 try {
-  EngineLoader.load("canvas", "Music", function () {
+  // TODO 这里测试indexedDB
+  EngineLoader.load("canvas", "File", function () {
     console.log('开始加载Defold引擎...');
     require("File_wasm.js")
   });

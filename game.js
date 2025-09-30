cf425b394f900e9e65626d3902dc052f9be700f5
diiyw <zhuohong@live.com>
2025-10-01 00:02:15 +0800

    fix(engine): REMOVE COMMENT

--- game.js
+++ game.js
@@ -1,9 +1,8 @@
 import "./src/weapp-adapter/index.js";
 import { EngineLoader } from './dmloader';
 try {
-  // TODO 这里测试indexedDB
   EngineLoader.load("canvas", "File", function () {
-    console.log('开始加载Defold引擎...');
+    console.log('开始加载引擎...');
     require("File_wasm.js")
   });
 } catch (error) {
